005bf75cfbf5e6962f5aab781708ee4848512897
dav- <derrick@cohodas.me>
2015-08-21 00:40:54 -0700

    Add test for Entity extending EventEmitter

--- test/spec/entity.spec.js
+++ test/spec/entity.spec.js
@@ -4,20 +4,33 @@
 
 describe('Entity', () => {
 
-  // Constructor
-  describe('Constructor', () => {
+  var Entity = SceneScroller.Entity
 
-    it('Creates a new Entity object', () => {
-      var e = new SceneScroller.Entity()
-      assert(e instanceof SceneScroller.Entity)
+  // Constructor
+  describe('constructor', () => {
+
+    var entity
+
+    beforeEach(() => {
+      entity = new Entity()
     })
 
-    it('Sets .created to Date object of creation time', () => {
+    it('creates a new Entity object', () => {
+      assert(entity instanceof Entity)
+    })
+
+    it('inherits from EventEmitter', () => {
+      assert(entity instanceof SceneScroller.EventEmitter)
+    })
+
+    it('sets its created property to the time the object was instantiated', () => {
       var t1 = new Date()
-      var e = new SceneScroller.Entity()
+      var e  = new SceneScroller.Entity()
       var t2 = new Date()
       expect(e.created).to.be.least(t1).and.most(t2)
+      assert(e.created instanceof Date)
     })
 
   })
+
 })
